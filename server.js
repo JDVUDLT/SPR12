83758802ec121a02de1f3a99809b66a0294762b6
Jadeov <igor.greatairbender@gmail.com>
2025-12-02 21:40:41 +0700

    Разметка сайта база

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const server = express();
 server.listen(3000);
 server.use(express.json());
+server.use(express.static(__dirname + "/public"));
 server.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
